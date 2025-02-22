ed64df9ecea045d8294b8661976d4acd1dd5e01a
Taneli <tvalimak@tvalimak.42.fr>
2025-02-22 19:40:16 +0200

    re-work

--- srcs/requirements/mariadb/db1.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mydatabase ;
-CREATE USER IF NOT EXISTS 'myuser'@'%' IDENTIFIED BY 'mysecurepassword' ;
-GRANT ALL PRIVILEGES ON mydatabase.* TO 'myuser'@'%' ;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass' ;
-FLUSH PRIVILEGES;
