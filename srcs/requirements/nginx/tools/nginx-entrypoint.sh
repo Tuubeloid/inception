e073684e467cdad85e6135b89f89514aacf397d8
Tuubeloid <107948842+Tuubeloid@users.noreply.github.com>
2025-03-06 09:42:55 +0200

    Update nginx-entrypoint.sh

--- srcs/requirements/nginx/tools/nginx-entrypoint.sh
+++ srcs/requirements/nginx/tools/nginx-entrypoint.sh
@@ -11,22 +11,27 @@ if [ -z "$DOMAIN_NAME" ] || [ -z "$SSL_CERT_PATH" ] || [ -z "$SSL_KEY_PATH" ]; t
   exit 1
 fi
 
-log "🔑 Checking if SSL certificates already exist..."
-if [ ! -f "$SSL_CERT_PATH" ] || [ ! -f "$SSL_KEY_PATH" ]; then
-  log "Generating self-signed SSL certificate and private key..."
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -out "$SSL_CERT_PATH" -keyout "$SSL_KEY_PATH" \
-  -subj "/C=FI/L=Helsinki/O=Hive/OU=Student/CN=$DOMAIN_NAME" > /dev/null 2>&1
-  
-  log "✅ SSL Certificates Generated"
+log "🔑 Checking if SSL certificates exist..."
+if [ -f "$SSL_CERT_PATH" ] && [ -f "$SSL_KEY_PATH" ]; then
+  log "✅ SSL certificates found — skipping generation."
 else
-  log "✅ SSL certificates already exist — skipping generation."
+  log "[ERROR] SSL certificates are missing!"
+  exit 1
 fi
 
+# Fix permissions in case they were modified
+log "🔒 Fixing SSL certificates permissions..."
+chmod 644 "$SSL_CERT_PATH" && chmod 600 "$SSL_KEY_PATH"
+
+# Replace placeholders in nginx.conf with env variables
 log "🔄 Replacing placeholders in nginx.conf with environment variables..."
 sed -i "s|ssl_cert_path|$SSL_CERT_PATH|g" /etc/nginx/nginx.conf
 sed -i "s|ssl_key_path|$SSL_KEY_PATH|g" /etc/nginx/nginx.conf
 sed -i "s|domain_name|$DOMAIN_NAME|g" /etc/nginx/nginx.conf
+
+# Debug list 🔥
+log "📄 Listing SSL certificate files for debug:"
+ls -la "$SSL_CERT_PATH" "$SSL_KEY_PATH"
 
 log "🚀 Starting Nginx service..."
 nginx -g "daemon off;"
