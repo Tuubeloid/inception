c50708d58cf8f95975c3f3ea2f56ada6c2be764a
Tuubeloid <107948842+Tuubeloid@users.noreply.github.com>
2025-03-07 17:36:23 +0200

    Update mariadb-entrypoint.sh

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -25,15 +25,16 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
 	echo "Setting up database and users..."
 	mysql --socket=/var/run/mysqld/mysqld.sock -uroot <<-EOF
-		FLUSH PRIVILEGES;
-		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
-		FLUSH PRIVILEGES;
+	    FLUSH PRIVILEGES;
+	    ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+	    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+	    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+	    FLUSH PRIVILEGES;
 	EOF
+
 
 	echo "Shutting down temporary MariaDB..."
 	mysqladmin shutdown --socket=/var/run/mysqld/mysqld.sock -uroot -p"${MYSQL_ROOT_PASSWORD}"
