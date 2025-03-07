54cb5dce071dcbc91a96ccdb19a0bf34457cceb6
Tuubeloid <107948842+Tuubeloid@users.noreply.github.com>
2025-03-07 16:49:31 +0200

    Update mariadb-entrypoint.sh

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -12,31 +12,36 @@ fi
 
 # Initialize database if it doesn't exist
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-  log "🔑 Initializing MariaDB Data Directory..."
-  mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null 2>&1
-  
-  log "✅ Database Directory Initialized"
-
-  log "🔥 Starting MariaDB temporarily to apply setup..."
-  mysqld --user=mysql --bootstrap <<EOF
-DROP DATABASE IF EXISTS $MYSQL_DATABASE;
-CREATE DATABASE $MYSQL_DATABASE;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-
--- Ensure the user exists
-DELETE FROM mysql.user WHERE User='$MYSQL_USER' AND Host='%';
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-  log "✅ Database Setup Completed"
-else
-  log "✅ Database Already Initialized — Skipping Setup"
+      log "🔑 Initializing MariaDB Data Directory..."
+      mysql_install_db --user=mysql --datadir=/var/lib/mysql
+      
+      log "✅ Database Directory Initialized"
+    
+      log "🔥 Starting MariaDB temporarily to apply setup..."
+      mysqld --user=mysql --skip-networking --socket=/var/run/mysqld/mysqld.sock &
+    
+      echo "Waiting for MariaDB to start..."
+    	until mysqladmin ping --socket=/var/run/mysqld/mysqld.sock --silent; do
+    	          sleep 2
+    	done
+    
+      log "🔥 Create table and user"
+    	mysql --socket=/var/run/mysqld/mysqld.sock -uroot <<-EOF
+    		FLUSH PRIVILEGES;
+    		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+    		FLUSH PRIVILEGES;
+    	EOF
+    
+      log "✅ Database Setup Completed"
+    else
+      log "✅ Database Already Initialized — Skipping Setup"
 fi
 
 log "🚀 Starting MariaDB service..."
 exec mysqld --user=mysql --console
 
-log "🚀 Starting MariaDB service..."
-exec mysqld --user=mysql --console
