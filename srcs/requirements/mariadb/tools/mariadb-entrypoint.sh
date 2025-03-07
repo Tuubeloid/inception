f88d262b372b8326af018aaf8fea5ae3b3891ff5
Tuubeloid <107948842+Tuubeloid@users.noreply.github.com>
2025-03-07 16:50:27 +0200

    Update mariadb-entrypoint.sh

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -10,38 +10,36 @@ if [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] 
   exit 1
 fi
 
-# Initialize database if it doesn't exist
+# configure server to be reachable by other containers on the first run
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-      log "🔑 Initializing MariaDB Data Directory..."
-      mysql_install_db --user=mysql --datadir=/var/lib/mysql
-      
-      log "✅ Database Directory Initialized"
-    
-      log "🔥 Starting MariaDB temporarily to apply setup..."
-      mysqld --user=mysql --skip-networking --socket=/var/run/mysqld/mysqld.sock &
-    
-      echo "Waiting for MariaDB to start..."
-    	until mysqladmin ping --socket=/var/run/mysqld/mysqld.sock --silent; do
-    	          sleep 2
-    	done
-    
-      log "🔥 Create table and user"
-    	mysql --socket=/var/run/mysqld/mysqld.sock -uroot <<-EOF
-    		FLUSH PRIVILEGES;
-    		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-    		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-    		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
-    		FLUSH PRIVILEGES;
-    	EOF
-    
-      log "✅ Database Setup Completed"
-    else
-      log "✅ Database Already Initialized — Skipping Setup"
+	echo "Installing MySQL..."
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+	echo "Starting MariaDB temporarily..."
+	mysqld --user=mysql --skip-networking --socket=/var/run/mysqld/mysqld.sock &
+
+	echo "Waiting for MariaDB to start..."
+	until mysqladmin ping --socket=/var/run/mysqld/mysqld.sock --silent; do
+		sleep 2
+	done
+
+	echo "Setting up database and users..."
+	mysql --socket=/var/run/mysqld/mysqld.sock -uroot <<-EOF
+		FLUSH PRIVILEGES;
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+		GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+		FLUSH PRIVILEGES;
+	EOF
+
+	echo "Shutting down temporary MariaDB..."
+	mysqladmin shutdown --socket=/var/run/mysqld/mysqld.sock -uroot -p"${MYSQL_ROOT_PASSWORD}"
 fi
 
+
 log "🚀 Starting MariaDB service..."
-exec mysqld --user=mysql --console
+exec mysqld --user=mysql
 
